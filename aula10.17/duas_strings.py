c4290ef6b58deb203dd9fad9784e3622fa2ba038
Felipe Morais <felipemsalgado5@gmail.com>
2023-10-17 16:43:45 -0300

    conteudo atualizado para ultilização do EaSCAPE

--- aula10.17/duas_strings.py
+++ aula10.17/duas_strings.py
@@ -1,8 +1,11 @@
+#você pode ESCAPAR aspas ultilizando aspas simples
+#vale reforçar que isso só vale para aspas duplas, outros caracteres não.
 #texto1 recebe o valor "Sou do"
 texto1 = "Sou do "
 
 #texto2 recebe o valor "Sou back-end"
-texto2 = "back-end"
+#caracter  de SCAPE é o \
+texto2 = "\"back-end\""
 
 # a função do print, concatena e imprime as duas variaveis na tela
 print (texto1, texto2)
