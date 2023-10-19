52dfa3c48d2e3779c75c19e203de805b361052fc
Felipe Morais <felipemsalgado5@gmail.com>
2023-10-19 17:39:10 -0300

    atividade02.py adicionada

---
+++ aula10.19/atividade02.py
@@ -0,0 +1,5 @@
+altura = input('Informe sua altura: ')
+altura_homem = (72.7 * float(altura)) -58
+altura_mulher = (62.1 * float(altura)) - 44.7
+print('Se você for homem, seu peso ideal é:', float(altura_homem))
+print('Se você for mulher, seu peso ideal é:', float(altura_mulher))
