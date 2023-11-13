b798ebe315b049bafdd140bca677cec5a3bd0cec
Felipe Morais <felipemsalgado5@gmail.com>
2023-11-13 14:52:13 -0300

    adicionado

---
+++ aula11.09/ambiente_virtual/aula01.py
@@ -0,0 +1,12 @@
+palavras = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+print(palavras[0:5])
+print(palavras[10:16])
+print(palavras[20:30])
+print(palavras[34:44])
+print(palavras[54:60])
+print(palavras[67:74])
+print(palavras[76:89])
+print(palavras[91:99])
+print(palavras[101:110])
+print(palavras[114:125])
---
+++ aula11.09/ambiente_virtual/aula02.py
@@ -0,0 +1,6 @@
+palavras = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+# uma informaçao cria pontes entre pessoas e ambientes
+# 8 palavras
+l1 = palavras[9:30]
+
+print(l1)
