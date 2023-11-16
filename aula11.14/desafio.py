a885160c522897515690c2670fa27208f9a54f37
Felipe Morais <felipemsalgado5@gmail.com>
2023-11-16 16:31:48 -0300

    desafio incompleto

--- aula11.14/desafio.py
+++ aula11.14/desafio.py
@@ -5,13 +5,24 @@ del lista_a[2]
 print(lista_a)
 
 # faça um codigo que cria uma lista de 10 numeros reais e imprima só os pares e depois só os impares.
-lista_b = []
+lista_b = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
 
 # faça um codigo que pede 3 notas de quatro alunos,calcula a media e printa as medias em uma lista.
-aluno1 = input("nota do aluno 1: ")
+# aluno1 = input("nota do aluno 1: ")
 
 
 # faça um codigo que pede 5 letras e quando for consoante ele avisa "é consoante"
-letras = input("Digite uma letra")
+lista_consoantes = []
+letras1 = input("Digite uma letra")
+letras2 = input("Digite uma letra")
+letras3 = input("Digite uma letra")
+letras4 = input("Digite uma letra")
+letras5 = input("Digite uma letra")
+
+
 
 # faça um codigo que pede 20 numeros inteiros e armazene os números pares na lista de pare e os numeros impares na lista de impar, printe as duas listas.
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+pares = numeros [2::1]
+print(pares)
