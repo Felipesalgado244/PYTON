117d24a81505ddd47de2b7c92e28e2b1437f4387
Felipe Morais <felipemsalgado5@gmail.com>
2023-12-07 14:02:25 -0300

    corrigido atv05.py

--- aula12.06/atv05.py
+++ aula12.06/atv05.py
@@ -1,8 +1,8 @@
 # VAmos criar um sistema de login e senha. crie um dicionario contendo os acessos dos colaboradores com nome de usuario e senha. em seguida peça as informações e valide o ligin do usuario.
-dic_acesso = {'felipe': '123456', 'paulo':'111111'}
+dic_acesso = {'felipe': 123456, 'paulo':111111}
 user_pass = {}
 usuario = input("Informe seu nome de usuario: ")
-senha = input("Informe sua senha: ")
+senha = int(input("Informe sua senha: "))
 
 user_pass[usuario] = senha
 
